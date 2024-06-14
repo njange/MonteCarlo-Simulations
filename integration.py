ea57c2285ef2e1bb9c517dadf661c5ec1d195553
njange <jamesnjange80@gmail.com>
2024-06-14 15:46:56 +0300

    integration

---
+++ integration.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+def monte_carlo_pi(num_samples):
+    inside_circle = 0
+    for _ in range(num_samples):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x**2 + y**2 <= 1:
+            inside_circle += 1
+    
+    return 4 * inside_circle / num_samples
+        
+#Estimate pi using 1,000,000 samples
+
