d46196c476f038ce255fae4251a0e5b1730ce3b3
njange <jamesnjange80@gmail.com>
2024-07-09 12:00:00 +0300

    Your commit message

--- integration.py
+++ integration.py
@@ -19,7 +19,7 @@ print(f"Estimated value of pi:{pi_estimate}")
 # The monte_carlo_pi function uses the Monte Carlo method
 #  to estimate the value of pi by generating random points
 #  in a square and counting how many fall within a quarter circle. 
-# The more points generated, the more accurate the estimate will be. 
+# The more points generated, the more accurate the estimate will be.
 # The estimated value of pi is calculated as 4 times the ratio of points 
 # inside the circle to the total number of points generated. 
 # The function returns this estimate. 
